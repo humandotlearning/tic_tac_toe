bab1675d4924e624fe32e68687ae5af1167d21c9
humandotlearning <nithin.varghese1357@gmail.com>
2026-04-13 15:04:35 +0530

    Initial commit

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Tic Tac Toe environment server components."""
+
+from .tic_tac_toe_environment import TicTacToeEnvironment
+
+__all__ = ["TicTacToeEnvironment"]
